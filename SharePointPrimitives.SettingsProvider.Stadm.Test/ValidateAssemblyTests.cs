3366c25156b4e6a8e5cbbd03dadbf5e816bce1e8
Chris Dibbs <cjdibbs@gmail.com>
2011-01-05 12:54:51 -0500

    test project

---
+++ SharePointPrimitives.SettingsProvider.Stadm.Test/ValidateAssemblyTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Specialized;
+
+namespace SharePointPrimitives.SettingsProvider.Stsadm.Test {
+    [TestClass]
+    public class ValidateAssemblyTests {
+        [TestMethod]
+        public void ValidateSimpleExamplea() {
+            var command = new ValidateAssembly();
+            string output;
+            StringDictionary args = new StringDictionary();
+            args["path"] = @"C:\Users\Chris\Desktop\SharePointPrimitives.SettingsProvider\SharePointPrimitives.SettingsProvider.SimpleExample\bin\Debug\SimpleExample.exe";
+            command.Run(null, args, out output);
+        }
+    }
+}
---
+++ SharePointPrimitives.SettingsProvider.Stsadm/install.ps1
@@ -0,0 +1,9 @@
+﻿Wspbuilder.exe
+
+stsadm -o retractsolution -name SharePointPrimitives.SettingsProvider.Stsadm.wsp -local
+stsadm -o execadmsvcjobs
+stsadm -o deletesolution -name  SharePointPrimitives.SettingsProvider.Stsadm.wsp
+
+stsadm -o addsolution -filename SharePointPrimitives.SettingsProvider.Stsadm.wsp
+stsadm -o deploysolution -name sharepointprimitives.settingsprovider.stsadm.wsp -local -allowgacdeployment
+stsadm -o execadmsvcjobs
